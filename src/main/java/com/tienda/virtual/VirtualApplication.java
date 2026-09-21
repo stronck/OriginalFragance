1f1ce8311e687b9503335cdc6e7dc62c1b998410
stronck <tibatacamilo2.0@gmail.com>
2026-09-21 16:19:18 -0500

    Add files via upload

---
+++ src/main/java/com/tienda/virtual/VirtualApplication.java
@@ -0,0 +1,20 @@
+// Clase principal que inicia la aplicación Spring Boot.
+package com.tienda.virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Autor: Camilo Tibatá Salguero
+ * Proyecto: E-commerce
+ * Descripción: spring boot.
+ */
+
+@SpringBootApplication
+public class VirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualApplication.class, args);
+	}
+
+}
