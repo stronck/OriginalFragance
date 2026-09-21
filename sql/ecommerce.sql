17c75853378948ec417e3d6962b21d07c5eb1c9d
stronck <tibatacamilo2.0@gmail.com>
2026-09-21 18:26:53 -0500

    Mejorar color de precios anteriores

--- sql/ecommerce.sql
+++ sql/ecommerce.sql
@@ -15,10 +15,10 @@ CREATE TABLE IF NOT EXISTS producto (
 
 -- Insertar datos en la tabla 'producto'
 INSERT INTO producto (nombre, descripcion, precio) VALUES
-('PACO RABANNE c- 1 FAME 80ml', 'Un perfume femenino con notas de menta, sangre de dragón y ámbar, ideal para ocasiones nocturnas. Duración de 12 a 24 horas. Envase dorado con detalle de medallón. <div><s style="font-size: 0.8em;">$649.999</s></div>', 599900.00),
-('PACO RABANNE c- 1 PURE XS BLACK 100ml', 'Un perfume masculino que combina notas de café y vainilla intensa, ofreciendo un aroma sensual con un toque de misterio y seducción. <div><s style="font-size: 0.8em;">$639.999</s></div>', 619900.00),
-('PACO RABANNE c- 1 LADY MILLION 80ml', 'Un perfume femenino, fresco, floral y seductor que combina neroli, jazmín, patchouli y especias, brindando un toque de glamour y sofisticación. <div><s style="font-size: 0.8em;">$529.999</s></div>', 499900.00),
-('PACO RABANNE c- 1 PURE XS 100ml', 'Un perfume masculino acuático y fresco que mezcla notas de pomelo, mandarina y flor de naranjo, aportando dinamismo y aventura. <div><s style="font-size: 0.8em;">$649.999</s></div>', 599900.00);
+('PACO RABANNE c- 1 FAME 80ml', 'Un perfume femenino con notas de menta, sangre de dragón y ámbar, ideal para ocasiones nocturnas. Duración de 12 a 24 horas. Envase dorado con detalle de medallón. <div><s style="font-size: 0.8em; color: #777;">$649.999</s></div>', 599900.00),
+('PACO RABANNE c- 1 PURE XS BLACK 100ml', 'Un perfume masculino que combina notas de café y vainilla intensa, ofreciendo un aroma sensual con un toque de misterio y seducción. <div><s style="font-size: 0.8em; color: #777;">$639.999</s></div>', 619900.00),
+('PACO RABANNE c- 1 LADY MILLION 80ml', 'Un perfume femenino, fresco, floral y seductor que combina neroli, jazmín, patchouli y especias, brindando un toque de glamour y sofisticación. <div><s style="font-size: 0.8em; color: #777;">$529.999</s></div>', 499900.00),
+('PACO RABANNE c- 1 PURE XS 100ml', 'Un perfume masculino acuático y fresco que mezcla notas de pomelo, mandarina y flor de naranjo, aportando dinamismo y aventura. <div><s style="font-size: 0.8em; color: #777;">$649.999</s></div>', 599900.00);
 
 -- Crear la tabla 'usuario'
 CREATE TABLE IF NOT EXISTS usuario (
