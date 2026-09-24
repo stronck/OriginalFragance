703038cda6d980d31ed05cd4148a6664dd3249f3
stronck <tibatacamilo2.0@gmail.com>
2026-09-24 06:49:27 -0500

    Documentar servicios y SQL

--- src/main/java/com/tienda/virtual/service/CarritoServiceImpl.java
+++ src/main/java/com/tienda/virtual/service/CarritoServiceImpl.java
@@ -1,3 +1,8 @@
+/*
+ * DOCUMENTACIÓN DETALLADA DEL ARCHIVO
+ * Este comentario explica la responsabilidad del componente y el flujo de datos que implementa.
+ * Todo el código funcional existente se conserva sin cambios; solamente se agregan comentarios.
+ */
 // Implementación del servicio que genera la factura PDF a partir del carrito.
 package com.tienda.virtual.service;
 
