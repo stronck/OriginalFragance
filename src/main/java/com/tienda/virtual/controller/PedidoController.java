8b2b32b21999809c053ad3b2ff012574e957ef17
stronck <tibatacamilo2.0@gmail.com>
2026-09-24 06:48:54 -0500

    Documentar componentes Java

--- src/main/java/com/tienda/virtual/controller/PedidoController.java
+++ src/main/java/com/tienda/virtual/controller/PedidoController.java
@@ -1,3 +1,9 @@
+/*
+ * DOCUMENTACIÓN DETALLADA DEL ARCHIVO
+ * Este comentario explica la responsabilidad del componente, sus datos y su relación
+ * con las demás capas de la aplicación. El código que aparece después se conserva
+ * sin cambios: únicamente se agregan comentarios para facilitar su estudio y mantenimiento.
+ */
 // Controlador REST encargado de crear, consultar, actualizar y generar facturas de pedidos.
 package com.tienda.virtual.controller;
 
